c361cb6d3b8b71c61ff8992d8b97fd02615f6750
Chandrakant Kumar <k.03chandra@gmail.com>
2016-11-05 23:47:41 +0530

    Use links instead of copy of config files

--- setup.sh
+++ setup.sh
@@ -7,13 +7,11 @@ echo "Updating repositories."
 sudo apt-get update
 
 echo "Installing dev tools."
-sudo apt-get install -y emacs vim git python-flake8 pep8 pyflakes
+sudo apt-get install -y emacs vim git python-flake8 pep8 pyflakes zsh
 
-echo "Configuring Emacs."
-mv ~/.emacs ~/.emacs.bak
-mv ~/.emacs.d ~/.emacs.d.bak
-mkdir ~/.emacs.d
-cp init.el ~/.emacs.d/init.el
+echo "Setting zsh"
+ln -s zshrc ~/.zshrc
+chsh -s zsh
 
 echo "Installing monaco font."
 wget https://github.com/todylu/monaco.ttf/raw/master/monaco.ttf -O ~/Downloads/monaco.ttf
@@ -22,3 +20,10 @@ if [ ! -d "~/.fonts" ]; then
 fi
 mv ~/Downloads/monaco.ttf ~/.fonts/
 fc-cache -f -v
+
+echo "Configuring Emacs."
+mv ~/.emacs ~/.emacs.bak
+mv ~/.emacs.d ~/.emacs.d.bak
+mkdir ~/.emacs.d
+ln -s emacs_init.el ~/.emacs.d/init.el
+emacs
