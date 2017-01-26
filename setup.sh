466b2eef2e567cceca7963348f3200e530993911
Chandrakant Kumar <k.03chandra@gmail.com>
2017-01-26 20:05:08 +0530

    Copy instead of link

--- setup.sh
+++ setup.sh
@@ -7,13 +7,11 @@ echo "Updating repositories."
 sudo apt-get update
 
 echo "Installing dev tools."
-sudo apt-get install -y emacs vim git python-flake8 pep8 pyflakes zsh
-
-echo "Installing i3"
-sudo apt-get install i3
+sudo apt-get install -y emacs vim git python-flake8 pep8 pyflakes zsh i3
 
 echo "Setting zsh"
-ln -s zshrc ~/.zshrc
+mv ~/.zshrc ~/.zshrc.bak
+cp zshrc ~/.zshrc
 chsh -s zsh
 
 echo "Installing monaco font."
@@ -28,11 +26,11 @@ echo "Configuring Emacs."
 mv ~/.emacs ~/.emacs.bak
 mv ~/.emacs.d ~/.emacs.d.bak
 mkdir ~/.emacs.d
-ln -s emacs_init.el ~/.emacs.d/init.el
+cp emacs_init.el ~/.emacs.d/init.el
 emacs
 
 echo "Configuring i3."
 mv ~/.i3 ~/.i3.bak
 mkdir ~/.i3
-ln -s i3config ~/.i3/config
-ln -s i3status.conf ~/.i3/i3status.conf
+cp i3config ~/.i3/config
+cp i3status.conf ~/.i3/i3status.conf
